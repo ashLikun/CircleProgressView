c0beeee1979ceabc1bded0ec79fa30477d5e24e1
likun <496546144@qq.com>
2019-06-03 11:08:16 +0800

    优化优化

--- app/src/main/java/com/ashlikun/circleprogress/simple/MainActivity.java
+++ app/src/main/java/com/ashlikun/circleprogress/simple/MainActivity.java
@@ -1,8 +1,8 @@
+package com.ashlikun.circleprogress.simple;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.ashlikun.circleprogress.CircleProgressView;
-import com.ashlikun.circleprogress.simple.R;
 
 public class MainActivity extends AppCompatActivity {
 
