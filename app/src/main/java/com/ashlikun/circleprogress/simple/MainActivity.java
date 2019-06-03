c8e2988817fe8550231f89c855cd25e371b1643e
likun <496546144@qq.com>
2019-06-03 11:00:01 +0800

    优化

--- app/src/main/java/com/ashlikun/circleprogress/simple/MainActivity.java
+++ app/src/main/java/com/ashlikun/circleprogress/simple/MainActivity.java
@@ -1,7 +1,9 @@
-package com.ashlikun.circleprogress.simple;
-
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.ashlikun.circleprogress.CircleProgressView;
+import com.ashlikun.circleprogress.simple.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        CircleProgressView view = findViewById(R.id.progressView);
+        view.setColor(0xff0000);
     }
 
 
